38b8a5371f1d7c7bbd06a3c973949e88970aef4f
zz3100201 <672516487@qq.com>
2015-07-24 15:44:52 +0800

    测试注释

--- app/src/main/java/com/zz/helloandroid/app/MainActivity.java
+++ app/src/main/java/com/zz/helloandroid/app/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    //hello
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
